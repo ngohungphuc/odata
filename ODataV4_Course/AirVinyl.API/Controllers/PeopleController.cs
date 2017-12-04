9ba6d5e327666a434cf49151b3ba2cb694b63b64
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-04 17:35:16 +0700

    update other operations

--- ODataV4_Course/AirVinyl.API/Controllers/PeopleController.cs
+++ ODataV4_Course/AirVinyl.API/Controllers/PeopleController.cs
@@ -8,6 +8,7 @@ using System.Web.OData;
 using System.Web.OData.Routing;
 using AirVinyl.API.Helpers;
 using AirVinyl.DataAccessLayer;
+using AirVinyl.Model;
 
 namespace AirVinyl.API.Controllers
 {
@@ -78,6 +79,139 @@ namespace AirVinyl.API.Controllers
             var collectionPropertyValue = person.GetValue(collectionPropertyToGet);
             return this.CreateOKHttpActionResult(collectionPropertyValue);
 
+        }
+
+        // alternative: attribute routing
+        // [HttpPost]
+        // [ODataRoute("People")]
+        // POST odata/People
+        public IHttpActionResult Post(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // add the person to the People collection
+            _context.People.Add(person);
+            _context.SaveChanges();
+
+            // return the created person 
+            return Created(person);
+        }
+
+        // PUT odata/People('key')
+        // alternative: attribute routing
+        // [HttpPut]
+        // [ODataRoute("People({key})")]
+        // PUT is for full updates
+        public IHttpActionResult Put([FromODataUri] int key, Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // PUT is for full updates: find the person
+            var currentPerson = _context.People.FirstOrDefault(p => p.PersonId == key);
+
+            if (currentPerson == null)
+            {
+                return NotFound();
+            }
+
+            // Alternative: if the person isn't found: Upsert.  This must only
+            // be used if the responsibility for creating the key isn't at 
+            // server-level.  In our case, we're using auto-increment fields,
+            // so this isn't allowed - code is for illustration purposes only!
+            //if (currentPerson == null)
+            //{
+            //    // the key from the URI is the key we should use
+            //    person.PersonId = key;
+            //    _context.People.Add(person);
+            //    _context.SaveChanges();
+            //    return Created(person);
+            //}
+
+            // if there's an ID property, this should be ignored. But if we try
+            // to call SetValues with a different Key value, SetValues will throw an error.
+            // Therefore, we set the person's ID to the key.
+            person.PersonId = currentPerson.PersonId;
+            _context.Entry(currentPerson).CurrentValues.SetValues(person);
+            _context.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // PATCH odata/People('key')
+        // alternative: attribute routing
+        // [HttpPatch]
+        // [ODataRoute("People({key})")]
+        // PATCH is for partial updates
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Person> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // find a matching person
+            var currentPerson = _context.People.FirstOrDefault(p => p.PersonId == key);
+
+            if (currentPerson == null)
+            {
+                return NotFound();
+            }
+
+            // Alternative: if the person isn't found: Upsert.  This must only
+            // be used if the responsibility for creating the key isn't at 
+            // server-level.  In our case, we're using auto-increment fields,
+            // so this isn't allowed - code is for illustration purposes only!
+            //if (currentPerson == null)
+            //{
+            //    var person = new Person();
+            //    person.PersonId = key;
+            //    patch.Patch(person);
+            //    _context.People.Add(person);
+            //    _context.SaveChanges();
+            //    return Created(person);
+            //}
+
+            // apply the changeset to the matching person
+            patch.Patch(currentPerson);
+            _context.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE odata/People('key')
+        // alternative: attribute routing
+        // [HttpDelete]
+        // [ODataRoute("People({key})")]
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            var currentPerson = _context.People.Include("Friends").FirstOrDefault(p => p.PersonId == key);
+            if (currentPerson == null)
+            {
+                return NotFound();
+            }
+
+            // this person might be another person's friend, we
+            // need to this person from their friend collections
+            var peopleWithCurrentPersonAsFriend =
+                _context.People.Include("Friends")
+                .Where(p => p.Friends.Select(f => f.PersonId).AsQueryable().Contains(key));
+
+            foreach (var person in peopleWithCurrentPersonAsFriend.ToList())
+            {
+                person.Friends.Remove(currentPerson);
+            }
+
+            _context.People.Remove(currentPerson);
+            _context.SaveChanges();
+
+            // return No Content
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
         protected override void Dispose(bool disposing)
