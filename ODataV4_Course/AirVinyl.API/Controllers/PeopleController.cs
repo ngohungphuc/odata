658277b7e6a57a2c0aa27ec0babcc509fcc0e383
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-04 16:48:56 +0700

    Selecting Properties and Raw Property Values

--- ODataV4_Course/AirVinyl.API/Controllers/PeopleController.cs
+++ ODataV4_Course/AirVinyl.API/Controllers/PeopleController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.OData;
 using System.Web.OData.Routing;
+using AirVinyl.API.Helpers;
 using AirVinyl.DataAccessLayer;
 
 namespace AirVinyl.API.Controllers
@@ -27,6 +29,55 @@ namespace AirVinyl.API.Controllers
                 return NotFound();
             }
             return Ok(person);
+        }
+
+
+        [HttpGet]
+        [ODataRoute("People({key})/Email")]
+        [ODataRoute("People({key})/FirstName")]
+        [ODataRoute("People({key})/LastName")]
+        [ODataRoute("People({key})/DateOfBirth")]
+        [ODataRoute("People({key})/Gender")]
+        public IHttpActionResult GetPersonProperty([FromODataUri] int key)
+        {
+            var person = _context.People.FirstOrDefault(p => p.PersonId == key);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            var propertyToGet = Url.Request.RequestUri.Segments.Last();
+            if (!person.HasProperty(propertyToGet))
+            {
+                return NotFound();
+            }
+
+            var propertyValue = person.GetValue(propertyToGet);
+
+            if (propertyValue == null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+
+            return this.CreateOKHttpActionResult(propertyValue);
+        }
+
+
+        [HttpGet]
+        [ODataRoute("People({key})/Friends")]
+        [ODataRoute("People({key})/VinylRecords")]
+        public IHttpActionResult GetPersonCollectionProperty([FromODataUri] int key)
+        {
+            var collectionPropertyToGet = Url.Request.RequestUri.Segments.Last();
+            var person = _context.People.Include(collectionPropertyToGet).FirstOrDefault(p => p.PersonId == key);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            var collectionPropertyValue = person.GetValue(collectionPropertyToGet);
+            return this.CreateOKHttpActionResult(collectionPropertyValue);
+
         }
 
         protected override void Dispose(bool disposing)
