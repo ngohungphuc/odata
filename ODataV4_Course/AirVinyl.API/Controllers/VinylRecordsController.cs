aec4d9fc1e77d58e745830a13e3d1fcf07a0fa45
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-04 15:34:01 +0700

    attribute base routing

---
+++ ODataV4_Course/AirVinyl.API/Controllers/VinylRecordsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using AirVinyl.DataAccessLayer;
+
+namespace AirVinyl.API.Controllers
+{
+    public class VinylRecordsController: ODataController
+    {
+        private AirVinylDbContext _context = new AirVinylDbContext();
+
+        [ODataRoute("VinylRecords")]
+        public IHttpActionResult Get()
+        {
+            return Ok(_context.VinylRecords);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
