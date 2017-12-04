5263c441155d70dac7119c35cb4b71e7c46182a6
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-04 15:05:48 +0700

    Selecting an EntitySet

---
+++ ODataV4_Course/AirVinyl.API/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+using AirVinyl.DataAccessLayer;
+
+namespace AirVinyl.API.Controllers
+{
+    public class PeopleController: ODataController
+    {
+        private AirVinylDbContext _context = new AirVinylDbContext();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_context.People);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
