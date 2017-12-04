580624eea44d983d6346af82e43c85e9254ac915
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-04 14:25:03 +0700

    Defining an Entity Data Model

---
+++ ODataV4_Course/AirVinyl.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using AirVinyl.Model;
+using Microsoft.OData;
+using Microsoft.OData.Edm;
+
+namespace AirVinyl.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapODataServiceRoute("OdataRoute", "odata", GetEdmModel());
+            config.EnsureInitialized();
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder
+            {
+                Namespace = "AirVinyl",
+                ContainerName = "AirVinylContainer"
+            };
+
+            builder.EntitySet<Person>("People");
+            builder.EntitySet<VinylRecord>("VinylRecords");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
